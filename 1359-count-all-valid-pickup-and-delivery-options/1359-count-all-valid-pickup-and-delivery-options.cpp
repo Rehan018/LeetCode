2a930065f53232b082c411ec7b426cc7643654b6
Md Rehan Alam <76740248+Rehan018@users.noreply.github.com>
2026-06-25 10:10:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (33.03%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOrders(int n) {
+
+        const long long MOD = 1e9 + 7;
+
+        long long ans = 1;
+
+        for (long long i = 1; i <= n; i++) {
+
+            ans = (ans * (2 * i - 1)) % MOD;
+            ans = (ans * i) % MOD;
+        }
+
+        return (int)ans;
+    }
+};
